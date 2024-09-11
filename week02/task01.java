8ed06d10c1304b902aed3b4ecf63e23bf52a0b39
sandy424 <126846886+sandy424@users.noreply.github.com>
2024-09-11 09:39:49 +0900

    자바2 week2

---
+++ week02/task01.java
@@ -0,0 +1,22 @@
+package Week02;
+
+import java.util.Scanner;
+
+public class task01 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("수 입력: ");
+		int score = scan.nextInt();
+		if(score >= 90) {
+			System.out.println("점수가 90보다 큽니다.");
+			System.out.println("등급은 A입니다.");
+		}
+		if(score < 90){
+			System.out.println("점수가 90보다 작습니다.");
+			System.out.println("등급은 B입니다.");
+		}
+	}
+
+}
