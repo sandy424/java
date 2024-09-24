4cbae5f109a538ac98af5a084166c6a4e90a4ba2
sandy424 <126846886+sandy424@users.noreply.github.com>
2024-09-24 10:00:56 +0900

    Add files via upload
    참조 타입

---
+++ task01.java
@@ -0,0 +1,13 @@
+package Week03;
+
+public class task01 {
+
+	public static void main(String[] args) {
+		int[] intArray = null;
+		//intArray[0] = 10;
+		
+		String str = null;
+		//System.out.println("총 문자수 : " + str.length());
+	}
+
+}
